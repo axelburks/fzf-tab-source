a61a8f0e66f8da80e63dccb74d69fad81b4c3342
axelburks <axel.burks@gmail.com>
2023-03-27 13:49:13 +0800

    ps: fix for some command truncated

--- sources/ps.zsh
+++ sources/ps.zsh
@@ -1,7 +1,7 @@
 # :fzf-tab:complete:(\\|*/|)(kill|ps):(*|argument-rest) --preview-window=down:4:wrap
 case $group in
 'process ID')
-  ps -p $word -w -o command=,%cpu=,%mem=,state=,start=,time= | bat --color=always -plsh
+  ps -ef $word | bat --color=always -plsh
   lsof -p $word -w
   ;;
 esac
